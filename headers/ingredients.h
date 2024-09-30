f95912d51ca0cfd38f02660cd43effaec6534772
christmas-fire <vpiskin335@gmail.com>
2024-09-30 21:37:47 +0700

    Объявили все основные классы ингредиентов

---
+++ headers/ingredients.h
@@ -0,0 +1,51 @@
+#ifndef INGREDIENTS_H
+#define INGREDIENTS_H
+
+class Ingredient {
+private:
+    int temperature; // температура
+    int amount; // количество
+    int time; // время, которое они требуют
+public:
+    Ingredient() : temperature(24), amount(0), time(0) {}
+    void setTemperature(int t);
+    int getTemperature() const;
+    void setAmount(int a);
+    int getAmount() const;
+    void setTime(int t);
+    int getTime() const;
+};
+
+class WetIngredient : public Ingredient {
+    // мокрые ингредиенты
+};
+ 
+class DryIngredient : public Ingredient {
+    // сухие ингредиенты
+};
+
+class CoffeeBeans : public DryIngredient {
+private:
+    bool is_grinded;
+public:
+    void setStatus(bool s);
+    bool getStatus() const;
+};
+
+class Sugar : public DryIngredient {
+    // сахар
+};
+
+class Milk : public WetIngredient {
+    // молоко
+};
+
+class Water : public WetIngredient {
+    // вода
+};
+
+class Cream : public WetIngredient {
+    // сливки
+};
+
+#endif // INGREDIENTS_H
