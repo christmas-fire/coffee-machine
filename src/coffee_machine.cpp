2654d82f0413e3d449bd629c1c7f4893fce28347
christmas-fire <vpiskin335@gmail.com>
2024-09-30 21:39:09 +0700

    Создали структуру для реализации классов кофемашины

---
+++ src/coffee_machine.cpp
@@ -0,0 +1,29 @@
+#include "coffee_machine.h"
+
+void CoffeeGrinder::grind() {
+    // 
+}
+
+void Group::boil() {
+    //
+}
+
+void Boiler::heat() {
+    // 
+}
+
+void SteamNozzle::steam() {
+    // 
+}
+
+void SteamNozzle::mix() {
+    // 
+}
+
+void SteamNozzle::add() {
+    // 
+}
+
+void CoffeeMachine::prepareCoffee() {
+    // 
+}
